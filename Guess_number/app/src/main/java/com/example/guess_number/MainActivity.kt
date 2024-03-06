5d5c3b6f56490fe224896e3085352929ba3189c8
CHWu <tarsiertail@gmail.com>
2024-03-07 07:16:23 +0800

    guess_number_v1

--- Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import android.widget.EditText
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +14,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val guess_button=findViewById<Button>(R.id.Guess_button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val guess_button = findViewById<Button>(R.id.Guess_button)
+        val reset_button = findViewById<Button>(R.id.reset_button)
+        val editText = findViewById<EditText>(R.id.editText)
+        var validate_num:Int
+        val MAXIMUM:Int=100
+        var maximum:Int =MAXIMUM
+        var minimum:Int =0
+        var secret: Int = Random().nextInt(maximum)+1
+
 
         guess_button.setOnClickListener{
-            Log.e("MainActivity","this is error")
+            validate_num=editText.text.toString().toInt()-secret
+            var ans_str: String="你猜對了喔"
 
+            if(validate_num>0){
+                maximum=editText.text.toString().toInt()
+                ans_str=minimum.toString()+"-"+maximum.toString()
+            }else if(validate_num<0){
+                minimum=editText.text.toString().toInt()
+                ans_str=minimum.toString()+"-"+maximum.toString()
+            }
+
+            textView.text=ans_str
+        }
+
+        reset_button.setOnClickListener{
+            maximum=MAXIMUM
+            minimum=0
+            secret = Random().nextInt(maximum)+1
+            textView.text=minimum.toString()+"-"+maximum.toString()
         }
     }
 }
